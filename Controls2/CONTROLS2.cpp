b1600a4b4a768bc14e105915d3686b25d15087cf
asoretmadolell <soret_40@hotmail.com>
2017-12-08 13:59:03 +0100

    Demonstrate radio buttons.

--- Controls2/CONTROLS2.cpp
+++ Controls2/CONTROLS2.cpp
@@ -1,4 +1,4 @@
-/* Demonstrates check boxes with enhancements. */
+/* Demonstrate radio buttons. */
 
 #include <Windows.h>
 //#include <string.h>
@@ -12,7 +12,8 @@ char szWinName[] = "MyWin"; /* name of window class */
 
 HINSTANCE hInst;
 
-int status1 = 0, status2 = 0; /* holds status of check boxes */
+int cbstatus1 = 0, cbstatus2 = 0; /* holds status of check boxes */
+int rbstatus1 = 1, rbstatus2 = 0; /* holds status of radio buttons */
 
 int WINAPI WinMain( HINSTANCE hThisInst, HINSTANCE hPreviInst, LPSTR lpszArgs, int nWinMode )
 {
@@ -45,7 +46,7 @@ int WINAPI WinMain( HINSTANCE hThisInst, HINSTANCE hPreviInst, LPSTR lpszArgs, i
     /* Now that a window class has been registered, a window can be created. */
     hwnd = CreateWindow(
         szWinName, /* name of window class */
-        "Using Check Boxes", /* title */
+        "Try Radio Buttons", /* title */
         WS_OVERLAPPEDWINDOW, /* window style - normal */
         CW_USEDEFAULT, /* X coordinate - let Windows decide */
         CW_USEDEFAULT, /* Y coordinate - let Windows decide */
@@ -83,7 +84,7 @@ int WINAPI WinMain( HINSTANCE hThisInst, HINSTANCE hPreviInst, LPSTR lpszArgs, i
 
 LRESULT CALLBACK WindowFunc( HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam )
 {
-    char str[ 80 ];
+    char str[ 255 ];
 
     switch( message )
     {
@@ -93,11 +94,15 @@ LRESULT CALLBACK WindowFunc( HWND hwnd, UINT message, WPARAM wParam, LPARAM lPar
                 case ID_DIALOG1:
                     DialogBox( hInst, "MYDB", hwnd, (DLGPROC)DialogFunc );
                     break;
-                case ID_STATUS: /* show check box status */
-                    if( status1 ) strcpy( str, "Checkbox 1 is checked\n" );
+                case ID_STATUS:
+                    if( cbstatus1 ) strcpy( str, "Checkbox 1 is checked\n" );
                     else strcpy( str, "Checkbox 1 is not checked\n" );
-                    if( status2 ) strcat( str, "Checkbox 2 is checked\n" );
+                    if( cbstatus2 ) strcat( str, "Checkbox 2 is checked\n" );
                     else strcat( str, "Checkbox 2 is not checked\n" );
+                    if( rbstatus1 ) strcat( str, "Radio 1 is checked\n" );
+                    else strcat( str, "Radio 1 is not checked\n" );
+                    if( rbstatus2 ) strcat( str, "Radio 2 is checked\n" );
+                    else strcat( str, "Radio 2 is not checked\n" );
                     MessageBox( hwnd, str, "", MB_OK );
                     break;
                 case ID_HELP:
@@ -121,9 +126,11 @@ LRESULT CALLBACK DialogFunc( HWND hdwnd, UINT message, WPARAM wParam, LPARAM lPa
     switch( message )
     {
         case WM_INITDIALOG:
-            /* The dialog box has just been displayed. Set the check boxes appropriately. */
-            SendDlgItemMessage( hdwnd, ID_CB1, BM_SETCHECK, status1, 0 );
-            SendDlgItemMessage( hdwnd, ID_CB2, BM_SETCHECK, status2, 0 );
+            /* The dialog box has just been displayed. Set the check boxes and radio buttons appropriately. */
+            SendDlgItemMessage( hdwnd, ID_CB1, BM_SETCHECK, cbstatus1, 0 );
+            SendDlgItemMessage( hdwnd, ID_CB2, BM_SETCHECK, cbstatus2, 0 );
+            SendDlgItemMessage( hdwnd, ID_RB1, BM_SETCHECK, rbstatus1, 0 );
+            SendDlgItemMessage( hdwnd, ID_RB2, BM_SETCHECK, rbstatus2, 0 );
             return 1;
         case WM_COMMAND:
             switch( LOWORD( wParam ) )
@@ -133,8 +140,13 @@ LRESULT CALLBACK DialogFunc( HWND hdwnd, UINT message, WPARAM wParam, LPARAM lPa
                     return 1;
                 case IDOK:
                     /* update global checkbox status variables */
-                    status1 = SendDlgItemMessage( hdwnd, ID_CB1, BM_GETCHECK, 0, 0 ); // is box checked?
-                    status2 = SendDlgItemMessage( hdwnd, ID_CB2, BM_GETCHECK, 0, 0 ); // is box checked?
+                    cbstatus1 = SendDlgItemMessage( hdwnd, ID_CB1, BM_GETCHECK, 0, 0 ); // is box checked?
+                    cbstatus2 = SendDlgItemMessage( hdwnd, ID_CB2, BM_GETCHECK, 0, 0 ); // is box checked?
+
+                    /*now, update global radio button status variables */
+                    rbstatus1 = SendDlgItemMessage( hdwnd, ID_RB1, BM_GETCHECK, 0, 0 ); // is button checked?
+                    rbstatus2 = SendDlgItemMessage( hdwnd, ID_RB2, BM_GETCHECK, 0, 0 ); // is button checked?
+
                     EndDialog( hdwnd, 0 );
                     return 1;
                 case ID_CB1: /* This is a manually managed check box. */
